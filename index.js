be95dd397affaf35d502903d1dc7d794d5f52915
DBRCDBRC <125450851+DBRCDBRC@users.noreply.github.com>
2023-02-15 15:52:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+
+
+const morgan = require("morgan");
+
+const app = express();
+const http = require("http").createServer(app);
+
+// db connection
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB CONNECTION ERROR: ", err));
+  
+// middlewares
+app.use(express.json({ limit: "4mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+
+// route middlewares
+app.use("/api", authRoutes);
+
+const port = process.env.PORT || 6000;
+
+http.listen(port, () => console.log("Server running on port 6000"));
